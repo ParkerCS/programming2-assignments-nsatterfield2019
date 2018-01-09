3ecf933cbba6675c81fe8ea5f1002870bae3d068
A Lee <alee@fwparker.org>
2018-01-08 19:31:39 -0600

    Add files via upload

---
+++ math_problems.py
@@ -0,0 +1,65 @@
+#SECTION 1 - MATH OPERATORS AND VARIABLES (31PTS TOTAL)
+
+# BASIC PROBLEMS 
+#PROBLEM 1 (From Math Class to Code - 5pts)
+# Print the answer to the math question:
+# 3(60x^2 + 3x/9) + 2x - 4/3(x) - sqrt(x)
+# where x = 12.83
+x = 12.83
+your_answer = "Enter the equation"
+print(your_answer)
+
+#PROBLEM 2 (Wholesale Books - 5pts)
+#The cover price of a book is $27.95, but bookstores get a 50 percent discount.
+#Shipping costs $4 for the first copy and 75 cents for each additional copy.
+# Calculate the total wholesale costs for 68 copies to the nearest penny.
+
+#PROBLEM 3 (Dining Room Chairs - 5pts)
+# You purchase eight chairs for your dining room.
+# You pay for the chairs plus sales tax at 9.5%
+# Make a program that prints the amount to the nearest penny using the variables below
+# Use the round(float, digits) function to get to nearest penny
+chair_price = 189.99
+tax_percent = 0.095
+units = 8
+
+#PROBLEM 4 (Area of Circle - 5pts)
+# Write code that can compute the area of circle.
+# Create variables for radius and pi (3.14159)
+# The formula, in case you do not know, is radius times radius times pi.
+# Print the outcome of your program as follows:
+# “The surface area of a circle with radius ... is ...”
+
+
+# INTERMEDIATE PROBLEMS
+#PROBLEM 2 (Set your alarm - 4pts)
+#You look at the clock and see that it is currently 1:00PM.
+# You set an alarm to go off 728 hours later.
+# At what time will the alarm go off? Write a program that prints the answer.
+# Hint: for the best solution, you will need the modulo operator.
+
+
+#PROBLEM 6 (Variable Swap - 4pts)
+# Can you think of a way to swap the values of two variables that does not
+# need a third variable as a temporary storage?
+# In the code below, try to implement the swapping of the values of 'a' and 'b' without using a third variable.
+# To help you out, the first step to do this is already given.
+# You just need to add two more lines of code.
+
+a = 17
+b = 23
+print( "a =", a, "and b =", b)
+a += b # this is the first line to help you out
+# add two more lines of code here to cause swapping of a and b
+print( "a =", a, "and b =", b)
+
+#ADVANCED PROBLEM
+#PROBLEM 7 (Coin counter - 3pts)
+# Write code that classifies a given amount of money (which you store in a variable named count),
+# as greater monetary units. Your code lists the monetary equivalent in dollars, quarters,
+# dimes, nickels, and pennies.
+# Your program should report the maximum number of dollars that fit in the amount,
+# then the maximum number of quarters that fit in the remainder after you subtract the dollars,
+# then the maximum number of dimes that fit in the remainder after you subtract the dollars and quarters,
+# and so on for nickels and pennies.
+# The result is that you express the amount as the minimum number of coins needed.
