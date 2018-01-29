20ef77b7fa48c8c7e7c9477e06309b81ff057d2a
A Lee <alee@fwparker.org>
2018-01-29 08:06:41 -0600

    Create loops_and_random.py

---
+++ loops_and_random.py
@@ -0,0 +1,23 @@
+# LOOPS (16pts TOTAL)
+
+# PROBLEM 1 (Fibonacci - 4pts)
+## The Fibonacci sequence is a sequence of numbers that starts with 1, followed by 1 again.
+# Every next number is the sum of the two previous numbers.
+# I.e., the sequence starts with 1, 1, 2, 3, 5, 8, 13, 21,...
+# Write a program that calculates and prints the Fibonacci sequence
+# until the numbers get higher than 1000.
+
+# PROBLEM 2 (Dice Sequence - 6pts)
+# You roll five six-sided dice, one by one.
+# What is the probability that the value of each die
+# is greater than OR EQUAL TO the value of the previous die that you rolled?
+# For example, the sequence “1, 1, 4, 4, 6” is a success,
+# but “1, 1, 4, 3, 6” is not. Determine the
+# probability of success using a simulation of a large number of trials.
+
+# PROBLEM 3 (Number Puzzler - 6pts)
+# A, B, C, and D are all different digits.
+# The number DCBA is equal to 4 times the number ABCD.
+# What are the digits?
+# Note: to make ABCD and DCBA conventional numbers, neither A nor D can be zero.
+# Use a quadruple-nested loop to solve. 
