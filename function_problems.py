e46c49a33bda7a391646a2a78281d7a0e24dd879
A Lee <alee@fwparker.org>
2018-01-31 06:05:39 -0600

    Create function_problems.py

---
+++ function_problems.py
@@ -0,0 +1,26 @@
+#FUNCTIONS (20PTS TOTAL)
+# Be sure to comment all your functions as shown in notes
+
+#PROBLEM 1 (how many upper case - 4pts)
+# Make a function takes a string as a parameter, then prints how many upper case letters are contained in the string.
+# A loop that compares each letter to the .upper() or .lower() of itself will suffice.
+# Make a call to that function using "Francis W. Parker" as the parameter (it should print "3")
+
+# PROBLEM 2 (Biggest, smallest, average - 4pts)
+# Make a function which takes 3 numbers as parameters.
+# The function then prints the largest, the smallest, and their average, rounded to 2 decimals.
+# Display the answers in a "nicely" formatted way.
+# Make a call to your function.
+
+# PROBLEM 4 (add me, multiply me - 4pts)
+# Make a function which takes in two integers and RETURNS their sum AND their product.
+# Make a call to the function, capture the product and sum as variables, and print each.
+
+# PROBLEM 5 (Login - 4pts)
+# Make a login function which works according to the flow diagram PasswordFlowchart.png in this folder
+# Substitute your name for Rohan's, and use whatever generic password you want.
+# Make a call to the function.
+
+# PROBLEM 6 (main - 4pts)
+# Create a main program using the format if __name__ == "__main__": 
+# Place every call from problems 1 through 5 into this program.
