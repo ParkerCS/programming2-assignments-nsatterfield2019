5a169214ee525d0fc2e224976c652a89618171c1
Nathan <nsatterfield@fwparker.org>
2018-02-06 13:24:37 -0600

    Updated

--- functions_and_imports.py
+++ functions_and_imports.py
@@ -7,15 +7,36 @@
 
 
 
+
 # PROBLEM 2 (Biggest, smallest, average - 4pts)
 # Make a function which takes 3 numbers as parameters.
 # The function then prints the largest, the smallest, and their average, rounded to 2 decimals.
 # Display the answers in a "nicely" formatted way.
 
+num1 = int(input("Insert a number: "))
+num2 = int(input("Insert a different number: "))
+num3 = int(input("Insert another different number: "))
 
+print("\n")
+
+average = (num1 + num2 + num3) // 3
+
+print("The average of the numbers is:", average)
+
+high = max(num1, num2, num3)
+print("The highest number is:" , high)
+
+low = min(num1, num2, num3)
+print("The lowest number is:" , low)
 
 # PROBLEM 4 (add me, multiply me - 4pts)
 # Make a function which takes in two integers and RETURNS their sum AND their product.
+
+def sum_product(n1, n2):
+    sum = n1 + n2
+    product = n1 * n2
+
+    return product, sum
 
 
 
