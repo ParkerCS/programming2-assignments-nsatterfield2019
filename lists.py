b7183415c3936a502286b4116a51d202725717d0
Nathan <nsatterfield@fwparker.org>
2018-02-16 09:03:47 -0600

    FINISHED

--- lists.py
+++ lists.py
@@ -1,11 +1,33 @@
 #LISTS (31PTS TOTAL)
 #In these exercises you should should use functions as needed.  All functions should have comments to describe their purpose.
 
+import random
+
 # PROBLEM 1 (Using List Comprehensions - 6pts)
 # Use the list comprehension method to do the following:
 # a) Make a list of numbers from 1 to 100
 # b) Make a list of even numbers from 20 to 40
 # c) Make a list of squares from 1 to 100 (1 ** 2 to 100 ** 2)
+
+numbers = []
+for i in range(1, 101):
+    numbers.append(i)
+
+print(numbers)
+
+
+even_num = []
+for i in range(20, 41):
+    if i % 2 == 0:
+        even_num.append(i)
+
+print(even_num)
+
+
+
+squares = [x ** 2 for x in range(100)]
+print(squares)
+
 
 #PROBLEM 2 (8-ball - 5pts)
 # A magic 8-ball, when asked a question, provides a random answer from a list.
@@ -19,11 +41,32 @@ now", "Cannot predict now", "Concentrate and ask again", "Don ' t \
 count on it", "My reply is no", "My sources say no", "Outlook \
 not so good", "Very doubtful" ]
 
+input("Ask a question:")
+print(random.choice(answer_list))
+
 
 # PROBLEM 3 (Shuffle - 5pts)
 # A playing card consists of a suit (Heart, Diamond, Club, Spade) and a value (2,3,4,5,6,7,8,9,10,J,Q,K,A).
 # Create a list of all possible playing cards, which is a deck.
 # Then create a function that shuffles the deck, producing a random order.
+
+suits = ["S", "H", "C", "D"]
+numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+deck = []
+for suit in suits:
+    for number in numbers:
+        deck.append(suit + number)
+
+print(deck)
+
+
+deck_new = []
+while len(deck) > 0:
+    print(len(deck_new))
+    draw = deck.pop(random.randrange(len(deck)))
+    deck_new.append(draw)
+print(deck_new)
 
 
 # PROBLEM 4 (Tic-Tac-Toe - 15pts)
@@ -60,6 +103,78 @@ not so good", "Very doubtful" ]
 #       announce draw
 #       break
 #   switch player
+
+def draw_board(draw):
+    print(draw[0][0], draw[0][1], draw[0][2])
+    print(draw[1][0], draw[1][1], draw[1][2])
+    print(draw[2][0], draw[2][1], draw[2][2])
+
+def check_win(board,player):
+    if board[0][0] == player and board[0][1] == player and board[0][2] == player: # row 1
+        return True
+
+    elif board[1][0] == player and board[1][1] == player and board[1][2] == player: # row 2
+        return True
+
+    elif board[2][0] == player and board[2][1] == player and board[2][2] == player: # row 3
+        return True
+
+    elif board[0][0] == player and board[1][0] == player and board[2][0] == player: # column 1
+        return True
+
+    elif board[0][1] == player and board[1][1] == player and board[2][1] == player: # column 2
+        return True
+
+    elif board[0][2] == player and board[1][2] == player and board[2][2] == player: # column 3
+        return True
+
+    elif board[0][0] == player and board[1][1] == player and board[2][2] == player: # diag 1 left to right
+        return True
+
+    elif board[0][2] == player and board[1][1] == player and board[2][0] == player: # diag 2 right to left
+        return True
+
+    else:
+        return False
+
+def check_full(board):
+    for row in board:
+        if " " in row:
+            return False
+    else:
+        return True
+
+done = False
+board = [[" ", " ", " "],[" ", " ", " "], [" ", " ", " "]]
+draw_board(board)
+player = "X"
+
+
+
+
+
+while not done:
+    row = int(input("player " + player + " Enter row (1,2,3): "))
+    col = int(input("player " + player + " Enter col (1,2,3): "))
+    if board[row-1][col-1] == " ":
+        board[row - 1][col - 1] = player
+    else:
+        print("That space is taken")
+        continue
+    draw_board(board)
+    if check_win(board,player):
+        print(player, "wins!!")
+        break
+    if check_full(board):
+        print("Draw!")
+        break
+    if player == "X":
+        player = "O"
+    else:
+        player = "X"
+
+
+
 
 # CHALLENGE PROBLEM 5 (MAY DO AS SUBSTITUTE FOR PROBLEM 4, NO ADDITIONAL CREDIT)
 # Create a program that is a simplified version of the game “Battleship.”
