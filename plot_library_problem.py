bb35b5db8d4a1cb071ffb1bdb9009344701adf77
A Lee <alee@fwparker.org>
2018-03-20 11:50:05 -0500

    Update and rename plot_library_problem.py to plotting_data_problems.py

--- plot_library_problem.py
+++
@@ -1,9 +0,0 @@
-
-# MATPLOTLIB PROBLEM # 1
-# Chicago Public Library Computer Usage (25pts)
-# open and read in the "2017_computer_sessions_chilib.csv" file into a list.
-# calculate (and make a list of) the total visitors to Chicago libraries each month.  Do not plot every library individually.  Find the total for all libraries and plot that.
-# Additionally, add lines for the three most visited libraries.
-# plot the total visitors on the y and month on the x.  You will have 4 separate lines (total and 3 libraries)
-# add a legend
-# label axes, title the graph
