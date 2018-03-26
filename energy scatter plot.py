d4847dfee96895976f3b88fc12c73cf0bfd6fccd
A Lee <alee@fwparker.org>
2018-03-26 11:11:43 -0500

    Create energy scatter plot.py

---
+++ energy scatter plot.py
@@ -0,0 +1,26 @@
+'''
+Energy Efficiency of Schools (35pts)
+
+https://data.cityofchicago.org/Environment-Sustainable-Development/Chicago-Energy-Benchmarking-2016-Data-Reported-in-/fpwt-snya\
+
+Chicago requires that all buildings over 50000 square feet in the city comply with energy benchmark reporting each year.
+The dataset at the link above is that data from 2016 which was reported in 2017.
+
+We will use this data to look at schools.  We will look to see if 
+
+Make a scatterplot which has the following:  
+- Plots the Total Greenhouse gas (GHG) Emmissions (y-axis), versus building square footage (x-axis) (15pts)
+- Includes ONLY data for K-12 Schools. (2pts)
+- Labelled x and y axis and appropriate title (3pt)
+- Annotated labels (school name) for the 5 highest and 5 lowest GHG Intensities. (3pts)
+- Label Francis W. Parker. (2pts)
+- Create a best fit line for schools shown. (5pts)
+- Customize your graph in a discernable way using any technique discussed or one from the API. (5pts)
+
+
+Challenge (for fun):
+- Make schools in top 10 percent of GHG Intensity show in green.
+- Make schools in bottom 10 percent GHG Intesity show in red.
+- Add colleges and universities (use a different marker type)
+
+'''
