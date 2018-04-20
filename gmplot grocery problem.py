6c18ca1938926661a6eed760d9670b60dcd2f46f
A Lee <alee@fwparker.org>
2018-04-19 22:05:41 -0500

    Create gmplot grocery problem.py

---
+++ gmplot grocery problem.py
@@ -0,0 +1,17 @@
+# In 2013, an interesting dataset was released for Chicago which identified a number of food deserts.
+# In many areas of the city, there were no suitable grocery stores with adequate produce, meats, refrigerated, and frozen food sections
+# Many areas were only served by corner stores and convenience stores.
+
+# Using gmplot and the chicago grocery csv, which contains updated grocery store data, 
+# create a map with the following characteristics.
+# - Map is centered on Chicago at a zoom level that shows all stores (5pts)
+# - All adequate grocery stores are plotted (could be scatterplot, circles, or markers) (use the one looks best to you) (10pts)
+#   Inadequate grocery stores are NOT plotted (you decide what inadequate means)
+# - Shows a heatmap which helps to visualize the greatest concentration of adequate stores. (10pts) 
+#   Your heatmap should be optimized for the city level view.  Tweak the radius and maxIntensity to adjust blob.
+
+
+# Challenges:  
+# Instead of a heatmap, make each store a circle with varying size based on square footage of the store.  
+# Filter out all liquor stores, drug stores, and convenience stores.
+# Place markers or other indicators where you still see inadequate food access.
