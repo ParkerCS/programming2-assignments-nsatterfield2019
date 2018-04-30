449b9f2d23c1a180d36226eb3a6450c9a8f0b80f
A Lee <alee@fwparker.org>
2018-04-30 06:52:13 -0500

    Create desktop calculator.py

---
+++ desktop calculator.py
@@ -0,0 +1,12 @@
+#  Make a desktop calculator app with pyqt  (37pts)
+#  Calculator will have the following functions and buttons. 
+#     Answer label where your equation and answer will appear.  It will be nicely formatted and update properly with every button push. (10pts)
+#     Clear button to zero your answer Label.  (2pts)
+#     buttons 0 through 9.   (5pts)
+#     *, /, -, and + buttons (5pts)
+#     = button to evaluate the current answer label. (5pts)
+#     Decimal button to add float capability (2pts)
+#     Will have the proper size.  All buttons will be of same relative size. (3pts)
+#     Use a stylesheet to change the color, font and size to approximately match the built in OS calulator app. (5pts)
+
+#  Model your calculator after the built in calc on your operating system.  (minus the +/- and % buttons)
